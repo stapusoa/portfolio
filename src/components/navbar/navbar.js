5e259ced0dc9cdd61411bcee41e6c9442e770d52
Sara Tapusoa <sara.tapusoa@gmail.com>
2023-03-01 23:38:05 -0700

    cards

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,6 +3,7 @@ import './navbar.css';
 import { useState } from 'react';
 import { AppBar, Toolbar, Typography, IconButton, Menu, MenuItem } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import Box from '@mui/material/Box';
 
 export default function Navbar() {
     const [anchorEl, setAnchorEl] = useState(null);
@@ -23,9 +24,9 @@ export default function Navbar() {
             <AppBar position="fixed" alignContent="center" elevation={0} sx={{ backgroundColor: 'rgba(255, 255, 255, 0.25)', backdropFilter: 'blur(5px)', boxShadow: '0px 1px 0px rgba(0, 0, 0, 0.15)' }}>
                 <Toolbar sx={{ height: '3vh', display: 'flex', justifyContent: 'space-between', paddingTop: '0.5vh' }}>
                     <div className="navbar-left">
-                        <div className="navbar-logo" alignContent="center">
+                        <div>
                             <a href="/pages/home.js">
-                                <img src={process.env.PUBLIC_URL + '/logo.svg'} alt="ST" sx={{ alignContent: 'center' }}/>
+                                <img src={process.env.PUBLIC_URL + '/logo.svg'} alt="ST" sx={{ paddingBottom: '1vh' }}/>
                             </a>
                         </div>
                         <div className="App-link">
@@ -44,34 +45,42 @@ export default function Navbar() {
                             >
                             <MenuIcon />
                         </IconButton>
-                        <Menu sx={{ borderRadius: '32px', boxShadow: '255 255 255 0.5'}}
-                            id="menu-appbar"
-                            anchorEl={anchorEl}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            open={open}
-                            onClose={handleMenuClose}
-                        >
-                            <MenuItem>
-                                <a href="#">About</a>
-                            </MenuItem>
-                            <MenuItem>
-                                <a href="#">Fishbowl Go App</a>
-                            </MenuItem>
-                            <MenuItem>
-                                <a href="#">Quarium Hosting Website</a>
-                            </MenuItem>
-                            <MenuItem>
-                                <a href="#">Travelpass Website</a>
-                            </MenuItem>
-                        </Menu>
+                        <Box sx={{ boxShadow: '0px 3px 10px rgba(220, 220, 226, 0.15)' }}>
+                            <Menu 
+                                PaperProps={{
+                                    sx: { 
+                                        borderRadius: '12px',
+                                        padding: '0.5vh 0',
+                                    },
+                                }}
+                                id="menu-appbar"
+                                anchorEl={anchorEl}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={open}
+                                onClose={handleMenuClose}
+                            >
+                                <MenuItem sx={{ padding: '2vh 3vw' }}>
+                                    <a href="#">About</a>
+                                </MenuItem>
+                                <MenuItem sx={{ padding: '2vh 3vw' }}>
+                                    <a href="#">Fishbowl Go App</a>
+                                </MenuItem>
+                                <MenuItem sx={{ padding: '2vh 3vw' }}>
+                                    <a href="#">Quarium Hosting Website</a>
+                                </MenuItem>
+                                <MenuItem sx={{ padding: '2vh 3vw' }}>
+                                    <a href="#">Travelpass Website</a>
+                                </MenuItem>
+                            </Menu>
+                        </Box>
                     </div>
                 </Toolbar>
             </AppBar>
