11fa6649ed5d6b4c89e550fdbf200acf60573bfc
Sara Tapusoa <79812543+stapusoa@users.noreply.github.com>
2024-10-05 00:16:09 -0600

    navbar setup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,23 @@
+import { Helmet } from 'react-helmet-async'
+import { Navbar } from '../components/Navbar/Navbar';
+
 const Home = () => {
+  console.log('Home component is rendering');
   return (
+    <>
+    <Helmet>
+        <title>Sara Tapusoa | UX Portfolio</title>
+        <meta property="og:title" content="Jane Doe | Web Developer Portfolio" />
+        <meta property="og:description" content="Explore my web development projects and skills in front-end and back-end technologies." />
+        <meta property="og:image" content="https://your-portfolio.com/images/portfolio-thumbnail.jpg" />
+            <link rel="canonical" href="https://www.saratapusoa.com" />
+      </Helmet>
+      <Navbar />
     <div>
       <h1>Home Page</h1>
       <p>Welcome to my portfolio!</p>
     </div>
+    </>
   );
 };
 
--- uno.config.ts
+++ uno.config.ts
@@ -9,6 +9,10 @@ export default defineConfig({
     },
   },
   shortcuts: {
-    'btn': 'px-4 py-2 rounded bg-primary text-white hover:bg-primary-700',
+    page: 'max-w-392 mx-auto px-4 md:px-6 lg:px-8',
+  'page-wide': 'max-w-452 mx-auto px-4 md:px-6',
+  'page-tight': 'max-w-300 mx-auto px-4 md:px-6 lg:px-8',
+  'page-tighter': 'max-w-4xl mx-auto px-4 md:px-6 lg:px-8',
+  'page-full': 'max-w-full',
   },
 })
