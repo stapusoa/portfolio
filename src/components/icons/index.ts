9e887ba91071b508918fe52b979f5916768845ed
Sara Tapusoa <79812543+stapusoa@users.noreply.github.com>
2024-12-08 14:35:15 -0700

    feat: add reusable icon components for improved consistency and scalability

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,1 +1,6 @@
 export { AccessibleIcon } from './AccessibleIcon'
+export { WhoIcon } from './WhoIcon'
+export { WhyIcon } from './WhyIcon'
+export { WhenIcon } from './WhenIcon'
+export { WhereIcon } from './WhereIcon'
+
