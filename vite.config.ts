167a5d6f64478db77c77a41206e20cf16ad4e588
Sara Tapusoa <79812543+stapusoa@users.noreply.github.com>
2024-12-10 21:30:09 -0700

    build config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,15 @@ import UnoCSS from 'unocss/vite'
 export default defineConfig({
   base: './',
   plugins: [react(), UnoCSS()],
-  
+  build: {
+    outDir: 'dist', // Output directory
+    sourcemap: true, // Enable source maps for debugging
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 });
