d44aa83b9e44966e54eadaec2d305f736decb5f3
Sara Tapusoa <79812543+stapusoa@users.noreply.github.com>
2024-12-13 09:41:26 -0700

    change anchors to link component

--- src/pages/sections/CaseStudies.tsx
+++ src/pages/sections/CaseStudies.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const CaseStudies: React.FC = () => (
   <>
     <div className="bg-transparent relative z-32 py-16 px-32 flex flex-row items-center">
@@ -23,9 +25,9 @@ const CaseStudies: React.FC = () => (
           </h6>
         </div>
         <div className='py-2 flex flex-row gap-1.5 items-center cursor-pointer group'>
-          <a href="/projects/project1" className="link hover:text-blue hover:border-blue">
+          <Link to="/projects/project1" className="link hover:text-blue hover:border-blue">
             case study
-          </a>
+          </Link>
           <div className='py-2 h-full'>
             <svg className='text-green group-hover:text-blue animate-pulse' style={{ animationDelay: '0s' }} width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="https://www.w3.org/2000/svg">
               <path d="M4.91967 14.0067C5.24633 14.3334 5.773 14.3334 6.09967 14.0067L11.6397 8.4667C11.8997 8.2067 11.8997 7.7867 11.6397 7.5267L6.09967 1.9867C5.773 1.66003 5.24633 1.66003 4.91967 1.9867C4.593 2.31337 4.593 2.84003 4.91967 3.1667L9.74633 8.00003L4.913 12.8334C4.593 13.1534 4.593 13.6867 4.91967 14.0067V14.0067Z" fill="currentColor" />
@@ -49,9 +51,9 @@ const CaseStudies: React.FC = () => (
           <h6 className='font-gilroy font-500 text-10 text-left text-grey-900 m-0 tracking-cozy'>providing professional therapeutic services through easy booking.</h6>
         </div>
         <div className='py-2 flex flex-row gap-1.5 items-center cursor-pointer group'>
-          <a href="/projects/project2" className="link hover:text-blue hover:border-blue">
+          <Link to="/projects/project2" className="link hover:text-blue hover:border-blue">
             case study
-          </a>
+          </Link>
           <div className='py-2 h-full'>
             <svg className='text-green group-hover:text-blue animate-pulse' style={{ animationDelay: '0s' }} width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="https://www.w3.org/2000/svg">
               <path d="M4.91967 14.0067C5.24633 14.3334 5.773 14.3334 6.09967 14.0067L11.6397 8.4667C11.8997 8.2067 11.8997 7.7867 11.6397 7.5267L6.09967 1.9867C5.773 1.66003 5.24633 1.66003 4.91967 1.9867C4.593 2.31337 4.593 2.84003 4.91967 3.1667L9.74633 8.00003L4.913 12.8334C4.593 13.1534 4.593 13.6867 4.91967 14.0067V14.0067Z" fill="currentColor" />
@@ -99,9 +101,9 @@ const CaseStudies: React.FC = () => (
           </h6>
         </div>
         <div className='py-2 flex flex-row gap-1.5 items-center cursor-pointer group'>
-          <a href="/projects/project3" className="link hover:text-blue hover:border-blue">
+          <Link to="/projects/project3" className="link hover:text-blue hover:border-blue">
             case study
-          </a>
+          </Link>
           <div className='py-2 h-full'>
             <svg className='text-green group-hover:text-blue animate-pulse' style={{ animationDelay: '0s' }} width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="https://www.w3.org/2000/svg">
               <path d="M4.91967 14.0067C5.24633 14.3334 5.773 14.3334 6.09967 14.0067L11.6397 8.4667C11.8997 8.2067 11.8997 7.7867 11.6397 7.5267L6.09967 1.9867C5.773 1.66003 5.24633 1.66003 4.91967 1.9867C4.593 2.31337 4.593 2.84003 4.91967 3.1667L9.74633 8.00003L4.913 12.8334C4.593 13.1534 4.593 13.6867 4.91967 14.0067V14.0067Z" fill="currentColor" />
