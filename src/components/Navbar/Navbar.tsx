c955b43108b6d82777ea04b594ec7fdd0110e12b
Sara Tapusoa <79812543+stapusoa@users.noreply.github.com>
2025-01-07 20:04:40 -0700

    fix: navbar responsiveness

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { Dialog, DialogPanel } from '@headlessui/react'
 import { Link } from 'react-router-dom';
 
 const navigation = [
@@ -12,83 +11,108 @@ const Navbar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   return (
     <>
-      <header className="sticky inset-x-0 top-0 z-50 bg-transparent">
-        <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
-          <div className="flex lg:flex-1">
-            <Link to="/" className="-m-1.5 p-1.5 cursor-pointer">
-              <span className="sr-only cursor-pointer">Your Company</span>
+      <header className="sticky top-0 z-50">
+        <nav className="flex items-center justify-between  mx-auto max-w-300 bg-transparent pt-5 pb-2 px-6 sm:px-6 md:px-14 lg:px-32 md:pt-9 lg:pt-11" aria-label="Global">
+          {/* Logo */}
+          <div className="flex items-center">
+            <Link to="/" className="flex items-center justify-center cursor-pointer">
               <img
-                alt=""
-                src='/images/logo-primary.webp'
                 className="h-5 w-auto cursor-pointer"
+                src='/images/logo-primary.webp'
+                alt="Sara Tapusoa Logo"
               />
             </Link>
           </div>
-          <div className="flex lg:hidden">
-            <button
-              type="button"
-              onClick={() => setMobileMenuOpen(true)}
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            >
-              <span className="sr-only">Open main menu</span>
-              <i aria-hidden="true" className="h-6 w-6" />
-            </button>
-          </div>
-          <div className="hidden lg:flex lg:gap-x-12">
+
+          {/* Desktop Navigation */}
+          <div className="hidden lg:flex gap-x-12">
             {navigation.map((item) => (
-              <Link key={item.name} to={item.href} className="navlink cursor-pointer">
+              <Link
+                key={item.name}
+                to={item.href}
+                className="navlink cursor-pointer"
+              >
                 {item.name}
               </Link>
             ))}
           </div>
+
+          {/* Mobile Menu Button */}
+          <div className="lg:hidden flex items-center">
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+              className="inline-flex items-center justify-center rounded-md border-2 border-transparent bg-transparent p-2 text-blue hover:bg-grey-100 active:bg-grey-100 hover:text-green active:text-green focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue"
+              aria-expanded={mobileMenuOpen}
+              aria-controls="mobile-menu"
+            >
+              <span className="sr-only">Open main menu</span>
+              {/* Hamburger Menu Icon */}
+              {mobileMenuOpen ? (
+                <svg
+                  className="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
         </nav>
-        <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-          <div className="fixed inset-0 z-50" />
-          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-            <div className="flex items-center justify-between">
-              <Link to="#" className="-m-1.5 p-1.5 cursor-pointer">
-                <span className="sr-only">Your Company</span>
-                <img
-                  alt=""
-                  src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-                  className="h-8 w-auto"
-                />
-              </Link>
-              <button
-                type="button"
-                onClick={() => setMobileMenuOpen(false)}
-                className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              >
-                <span className="sr-only">Close menu</span>
-                <i aria-hidden="true" className="h-6 w-6" />
-              </button>
+
+        {/* Mobile Menu */}
+        {mobileMenuOpen && (
+          <div className="absolute mt-3 w-full border-t-2 border-x-0 border-b-0 border-solid border-green flex lg:hidden bg-white/20 backdrop-blur-2xl" id="mobile-menu">
+            <div className="space-y-6 pb-full w-full pt-8">
+              {navigation.map((item) => (
+                <Link
+                  key={item.name}
+                  to={item.href}
+                  onClick={() => setMobileMenuOpen(false)} // Close menu on link click
+                  className="block border-l-4 w-full border-transparent text-align-left decoration-none py-2 px-6 sm:px-6 md:px-14 lg:px-32 font-gilroy text-7 font-medium text-gray-500 hover:border-green active:border-green hover:bg-grey-200 active:bg-grey-200 hover:text-green active:text-green"
+                >
+                  {item.name}
+                </Link>
+              ))}
             </div>
-            <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/10">
-                <div className="space-y-2 py-6">
-                  {navigation.map((item) => (
-                    <Link
-                      key={item.name}
-                      to={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base cursor-pointer font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                    >
-                      {item.name}
-                    </Link>
-                  ))}
-                </div>
-                <div className="py-6">
-                  <Link
-                    to="#"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold cursor-pointer leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    Log in
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </DialogPanel>
-        </Dialog>
+
+          </div>
+        )}
       </header>
+
+
+
+
+
+
+
+
+
     </>
   )
 }
